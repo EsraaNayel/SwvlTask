604cd47f831a94a26857c6fce0b04362a73533b4
Esraa Nayel <esraa.nayel1@gmail.com>
2020-08-24 02:20:34 +0200

    -movies list categorized by year

--- app/src/main/java/com/en/tech/ui/main/adapter/MainAdapter.kt
+++ app/src/main/java/com/en/tech/ui/main/adapter/MainAdapter.kt
@@ -1,46 +1,81 @@
 package com.en.tech.ui.main.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.en.tech.databinding.MovieMainItemBinding
+import com.en.tech.App.Companion.context
+import com.en.tech.R
 import com.en.tech.domain.models.Movie
+import kotlinx.android.synthetic.main.movie_main_item.view.*
+import kotlinx.android.synthetic.main.year_item.view.*
+
+private const val VIEW_TYPE_HEADER = 1
+private const val VIEW_TYPE_MOVIE = 2
 
 class MainAdapter(
     private val context: Context,
     private val onMovieClicked: (Movie) -> Unit
-) : RecyclerView.Adapter<MainAdapter.MovieViewHolder>() {
-    private var movieList: List<Movie>? = null
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var items: List<Any> = emptyList()
 
-    fun updateMovieList(movieList: List<Movie>) {
-        this.movieList = movieList
+    fun updateMovieList(items: List<Any>) {
+        this.items = items
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val itemBinding =
-            MovieMainItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MovieViewHolder(itemBinding, onMovieClicked)
-    }
-
-    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        holder.bindHomeMovies(movieList!![position], context)
-    }
-
     override fun getItemCount(): Int {
-        return if (movieList.isNullOrEmpty()) 0 else movieList!!.size
+        return items.size
     }
 
-    class MovieViewHolder(
-        private val binding: MovieMainItemBinding,
-        private val onMovieClicked: (Movie) -> Unit
-    ) : RecyclerView.ViewHolder(binding.root) {
-        fun bindHomeMovies(movie: Movie, context: Context) {
-            binding.movieTitle.text = movie.title
-            binding.rating.text = movie.rating.toString()
-            binding.releaseDate.text = movie.year.toString()
-            binding.item.setOnClickListener { onMovieClicked(movie) }
+    override fun getItemViewType(position: Int): Int {
+        val item = items[position]
+        return if (item is Int)
+            VIEW_TYPE_HEADER
+        else {
+            VIEW_TYPE_MOVIE
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == VIEW_TYPE_HEADER) {
+            HeaderViewHolder(
+                LayoutInflater.from(context).inflate(R.layout.year_item, parent, false)
+            )
+        } else {
+            MovieItemViewHolder(
+                LayoutInflater.from(context).inflate(R.layout.movie_main_item, parent, false)
+            )
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = items[position]
+        if (holder is HeaderViewHolder) {
+            holder.bind(item as Int)
+        } else if (holder is MovieItemViewHolder) {
+            holder.bind(item as Movie)
+        }
+    }
+
+    inner class HeaderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private var releaseDate: TextView = view.release_date
+
+        fun bind(year: Int) {
+            releaseDate.text = year.toString()
+        }
+    }
+
+    inner class MovieItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private var movieTitle: TextView = view.movie_title
+        private var rating: TextView = view.rating
+
+        fun bind(movie: Movie) {
+            movieTitle.text = movie.title
+            rating.text = movie.rating.toString()
         }
     }
 }
